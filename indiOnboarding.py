0e5f62229adcca911e7d41e149785d3624ae319c
Brian Chan <alto1020@gmail.com>
2016-08-15 13:54:43 -0700

    Modified how to add and change indibots user info

--- indiOnboarding.py
+++ indiOnboarding.py
@@ -8,11 +8,6 @@ def executeDBCommand(conn, cur, query):
     cur.execute(query)
     conn.commit()
 
-def promptForUserId():
-    userId = raw_input("Enter user ID: ")
-    #user check
-    return userId
-
 def promptForName():
     name = raw_input("Enter the new user's name: ")
     return name
@@ -50,7 +45,30 @@ def addNewUser():
     global conn
     global cur
 
-    userId = promptForUserId()
+    queryStr = "SELECT DISTINCT userID FROM unprocessedmessages WHERE influencerid = 'indibot' AND userid NOT IN (SELECT userid FROM indiuserinfo) ORDER BY userid;"
+    executeDBCommand(conn, cur, queryStr)
+    ids = cur.fetchall()
+    validids = []
+    i = len(ids) - 1
+    for row in ids:
+        print str(i) + ": " + row[0]
+        validids.append(str(row[0]))
+        i -= 1
+    editId = raw_input("Input index of userId to enter: ")
+    editId = len(validids) - (1 + int(editId))
+    if editId < 0 or editId >= len(validids):
+        print 'Not a valid index\n'
+        return
+    userId = validids[editId]
+
+    queryStr = "SELECT phrase FROM unprocessedmessages WHERE userid = '" + userId + "' AND influencerid = 'indibot';"
+    print '\n'
+    executeDBCommand(conn, cur, queryStr)
+    convo = cur.fetchall()
+    for row in convo:
+        print row[0]
+    print '\n'
+    #userId = promptForUserId()
     name = promptForName()
     gender = promptForGender()
     height = promptForHeight()
@@ -58,61 +76,81 @@ def addNewUser():
     goals = promptForGoals()
     days = promptForDays()
 
-    queryStr = "INSERT INTO indiuserinfo VALUES ( DEFAULT, '" + userId + "', '" + name + "', '" + gender + "', " + str(height) + ", " + str(weight) + ", '" + goals + "', '" +  days + "');"
+    queryStr = "INSERT INTO indiuserinfo VALUES (DEFAULT, '" + userId + "', '" + name + "', '" + gender + "', " + str(height) + ", " + str(weight) + ", '" + goals + "', '" +  days + "');"
     executeDBCommand(conn, cur, queryStr)
+
+    print 'New user succesfully entered\n'
 
 def changeUserInfo():
     global conn
     global cur
 
-    user = promptForUserId()
-    #TODO: User check
+    queryStr = "SELECT DISTINCT userid FROM indiuserinfo ORDER BY userid;"
+    executeDBCommand(conn, cur, queryStr)
+    ids = cur.fetchall()
+    validids = []
+    i = len(ids) - 1
+    for row in ids:
+        print str(i) + ": " + row[0]
+        validids.append(str(row[0]))
+        i -= 1
+    editId = raw_input("Input index of userId to enter: ")
+    editId = len(validids) - (1 + int(editId))
+    if editId < 0 or editId >= len(validids):
+        print 'Not a valid index\n'
+        return
+    userId = validids[editId]
+
+    queryStr = "SELECT * FROM indiuserinfo WHERE userid = '" + userId + "';"
+    executeDBCommand(conn, cur, queryStr)
+    users = cur.fetchall()
+    if len(users) == 0:
+        print '\nInvalid user ID\n'
+        return
 
     while True:
-        print "0: Name"
+        print "\n0: Name"
         print "1: Gender"
         print "2: Height"
         print "3: Weight"
         print "4: Goals"
         print "5: Days\n"
 
-        option = raw_input("Select index of info you want to change: ")
+        option = raw_input("Select index of info you want to change ('q' to quit): ")
 
         if option == '0':
             name = promptForName()
-            queryStr = "UPDATE indiuserinfo SET name = '" + name + "' WHERE userid = '" + userId "';"
-            executeDBCommand(conn, cur, queryStr)
+            queryStr = "UPDATE indiuserinfo SET name = '" + name + "' WHERE userid = '" + userId + "';"
             break
         elif option == '1':
             gender = promptForGender()
-            queryStr = "UPDATE indiuserinfo SET gender = '" + gender + "' WHERE userid = '" + userId "';"
-            executeDBCommand(conn, cur, queryStr)
+            queryStr = "UPDATE indiuserinfo SET gender = '" + gender + "' WHERE userid = '" + userId + "';"
             break
         elif option == '2':
             height = promptForHeight()
-            queryStr = "UPDATE indiuserinfo SET height = " + str(height) + " WHERE userid = '" + userId "';"
-            executeDBCommand(conn, cur, queryStr)
+            queryStr = "UPDATE indiuserinfo SET height = " + str(height) + " WHERE userid = '" + userId + "';"
             break
         elif option == '3':
             weight = promptForWeight()
-            queryStr = "UPDATE indiuserinfo SET weight = " + str(weight) + " WHERE userid = '" + userId "';"
-            executeDBCommand(conn, cur, queryStr)
+            queryStr = "UPDATE indiuserinfo SET weight = " + str(weight) + " WHERE userid = '" + userId + "';"
             break
         elif option == '4':
             goals = promptForGoals()
-            queryStr = "UPDATE indiuserinfo SET goals = '" + goals + "' WHERE userid = '" + userId "';"
-            executeDBCommand(conn, cur, queryStr)
+            queryStr = "UPDATE indiuserinfo SET goals = '" + goals + "' WHERE userid = '" + userId + "';"
             break
         elif option == '5':
             days = promptForDays()
-            queryStr = "UPDATE indiuserinfo SET days = '" + days + "' WHERE userid = '" + userId "';"
-            executeDBCommand(conn, cur, queryStr)
+            queryStr = "UPDATE indiuserinfo SET days = '" + days + "' WHERE userid = '" + userId + "';"
             break
+        elif option == 'q':
+            return
         else:
             print 'ERROR: Invalid input, try again\n'
+    executeDBCommand(conn, cur, queryStr)
+    print '\nUser info succesfully changed'
 
 def printOptions():
-    print "0: Set-up entire new user"
+    print "\n0: Set-up entire new user"
     print "1: Change user's info\n"
 
 if __name__ == '__main__':
@@ -128,7 +166,7 @@ if __name__ == '__main__':
 
     while True:
         printOptions()
-        category = raw_input("Enter task number ('q' to exit): ")
+        category = raw_input("Enter task number ('q' to quit): ")
         if category == '0':
             addNewUser()
         elif category == '1':
